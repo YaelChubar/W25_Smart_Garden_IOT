1d9e77563094c65d33008250543c6ca1fa11ec43
Yael Chubar <yaelch@Yaels-MacBook-Pro-2.local>
2025-01-14 18:36:19 +0200

    add is plant ready logic

--- project/main/main.ino
+++ project/main/main.ino
@@ -20,7 +20,10 @@ Adafruit_NeoPixel pixels(NUMPIXELS, LEDS_PIN, NEO_GRB + NEO_KHZ800);
 // Functions
 void initial_wifi_setup();
 void firebase_setup();
-void check_calibration(int plant_id);
+int get_moisture_sensor_pin_by_id(int plant_id);
+void check_calibration_mode(int plant_id);
+bool is_plant_ready(int plant_id);
+void water_plant(int plant_id);
 //void get_existing_plants();
 
 void setup() {
@@ -79,14 +82,16 @@ void loop() {
     if (Firebase.RTDB.getJSON(&fbdo, garden_path)) {
       for (int i = 1; i < 5; i++) {
         //for each plant, check:        
-        // Calibration mode
-        // Is exists
-        // Moisture sensor
-        check_calibration(i);
+        // 1. Calibration mode
+        // 2. Is ready
+        // 3. Moisture sensor
+        check_calibration_mode(i);
         // if not in calibration mode, check if ready
-        // if not ready
-        // continue
-        // else - check moisture and water if needed
+        if (!is_plant_ready(i)) {
+          continue;
+        }
+        // check moisture and water if needed
+        water_plant(i);
       }
       
       // Garden exists in firebase
@@ -108,27 +113,26 @@ void loop() {
 
 }
 
-void check_calibration(int plant_id) {
-  int moisture_pin = 0;
+int get_moisture_sensor_pin_by_id(int plant_id) {
   switch (plant_id) {
     case 1:
-      moisture_pin = MOISTURE_SENSOR_PIN_1;
-      break;
+      return MOISTURE_SENSOR_PIN_1;
     case 2:
-      moisture_pin = MOISTURE_SENSOR_PIN_2;
-      break;
+      return MOISTURE_SENSOR_PIN_2;
     case 3:
-      moisture_pin = MOISTURE_SENSOR_PIN_3;
-      break;
+      return MOISTURE_SENSOR_PIN_3;
     case 4:
-      moisture_pin = MOISTURE_SENSOR_PIN_4;
-      break;
+      return MOISTURE_SENSOR_PIN_4;
     default:
       Serial.print("Invalid plant_id! \n");
-      return;
+      return -1;
   }
-  
+  return -1;
+}
+
+void check_calibration_mode(int plant_id) {
   String plant_calibration_path = garden_path + "/plants/plant" + String(plant_id)+ "/calibration";
+  int moisture_pin = get_moisture_sensor_pin_by_id(plant_id);
   int moisture_sensor_reading = 0;
 
   if (Firebase.RTDB.getJSON(&fbdo, plant_calibration_path)) {
@@ -216,15 +220,124 @@ void check_calibration(int plant_id) {
   
 }
 
+bool is_plant_ready(int plant_id) {
+  String plant_path = garden_path + "/plants/plant" + String(plant_id);
+  if (Firebase.RTDB.getJSON(&fbdo, plant_path)) {
+    FirebaseJson &json = fbdo.jsonObject();
+    FirebaseJsonData is_plant_ready;
+    
+    if (json.get(is_plant_ready, "is_plant_ready")) { 
+      if(is_plant_ready.intValue == 1) {
+        Serial.printf("plant %d is ready\n", plant_id);
+        return true;
+      } else {
+        Serial.printf("plant %d is NOT ready\n", plant_id);
+        return false;
+      }
+    }
+  } else {
+    // TODO: add error indication in firebase
+    Serial.printf("Error getting plant %d directory: %s\n", plant_id, fbdo.errorReason().c_str());
+  }
+  return false;
+}
+
+
+/* 
+Flow: 
+1. Read moisture calibration dry and wet values
+2. Read soil moisture level (1/2/3)
+3. get moisture pin according to plant id
+4. Preform sensor reading
+5. normalize reading to precent
+6. If value is lower than the one set in soil moisture level, pump water for 5 seconds
+*/
+void water_plant(int plant_id) {
+  String plant_path = garden_path + "/plants/plant" + String(plant_id);
+  String plant_calibration_path = garden_path + "/plants/plant" + String(plant_id)+ "/calibration";
+  int moisture_pin = get_moisture_sensor_pin_by_id(plant_id);
+  int dry_soil_val = 0;
+  int wet_soil_val = 0;
+  int soil_moisture_level_val = 0;
+  int moisture_sensor_reading = 0;
+
+  // Read moisture calibration dry and wet values
+  if (Firebase.RTDB.getJSON(&fbdo, plant_calibration_path)) {
+    FirebaseJson &json = fbdo.jsonObject();
+    FirebaseJsonData dry_soil_measurment;
+    FirebaseJsonData wet_soil_measurment;
+
+    if (json.get(dry_soil_measurment, "dry_soil_measurment")) {
+      Serial.print("dry_soil_measurment: ");
+      Serial.println(dry_soil_measurment.intValue);
+      dry_soil_val = dry_soil_measurment.intValue;
+    } else {
+      Serial.printf("Error getting plant %d dry_soil_measurment: %s\n", plant_id, fbdo.errorReason().c_str());
+      return;
+    }
+
+    if (json.get(wet_soil_measurment, "wet_soil_measurment")) {
+      Serial.print("wet_soil_measurment: ");
+      Serial.println(wet_soil_measurment.intValue);
+      wet_soil_val = wet_soil_measurment.intValue;
+    } else {
+      Serial.printf("Error getting plant %d wet_soil_measurment: %s\n", plant_id, fbdo.errorReason().c_str());
+      return;
+    }
+  } else {
+    Serial.printf("Error getting plant %d plant_calibration_path directory: %s\n", plant_id, fbdo.errorReason().c_str());
+    return;
+  }
+
+  //Read soil moisture level (1/2/3)
+  if (Firebase.RTDB.getJSON(&fbdo, plant_path)) {
+    FirebaseJson &json = fbdo.jsonObject();
+    FirebaseJsonData soil_moisture_level;
+
+    if (json.get(soil_moisture_level, "soil_moisture_level")) {
+      Serial.print("soil_moisture_level: ");
+      Serial.println(soil_moisture_level.intValue);
+  
+      switch (soil_moisture_level.intValue) {
+        case 1:
+          soil_moisture_level_val = 10;
+        case 2:
+          soil_moisture_level_val = 30;
+        case 3:
+          soil_moisture_level_val = 50;
+        default:
+          Serial.printf("Invalid soil moisture val for plant %d: %s\n", plant_id, fbdo.errorReason().c_str());
+          return;
+      }
+
+    } else {
+      Serial.printf("Error getting plant %d soil_moisture_level: %s\n", plant_id, fbdo.errorReason().c_str());
+      return;
+    }
+  } else {
+    Serial.printf("Error getting plant %d plant_path directory: %s\n", plant_id, fbdo.errorReason().c_str());
+    return;
+  }
+
+
+}
+
+
+
+
+
+
+
+
 // void get_existing_plants() {
 //   for (int i = 0; i < 4; i++) {
 //     String plant_path = garden_path + "/plants/plant" + String(i+1);
 //     if (Firebase.RTDB.getJSON(&fbdo, plant_path)) {
 //       FirebaseJson &json = fbdo.jsonObject();
-//       FirebaseJsonData is_plant_exists;
+//       FirebaseJsonData is_plant_ready;
       
-//       if (json.get(is_plant_exists, "is_plant_exists")) { 
-//         existing_plants[i] = is_plant_exists.intValue;
+//       if (json.get(is_plant_ready, "is_plant_ready")) { 
+//         existing_plants[i] = is_plant_ready.intValue;
 //       }
 //     } else {
 //       // TODO: add error indication in firebase
